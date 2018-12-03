d4bf1b025d22b1ca32767fe136c72a00a13a0298
rebelskum <jette.hernan@gmail.com>
2018-12-02 23:42:56 -0800

    added some visible features and fixed bugs

--- api/app/controllers/words_controller.rb
+++ api/app/controllers/words_controller.rb
@@ -4,7 +4,8 @@ class WordsController < ApplicationController
   def index
     url = "http://app.linkedin-reach.io/words"
     resp = Net::HTTP.get_response(URI.parse(url))
-    words = resp.body.split("\n")
+    words = resp.body
     render :json => (words)
   end
 end
+# i need a serve js file in rails
--- api/public/web/hangman.js
+++ api/public/web/hangman.js
@@ -24,11 +24,11 @@ let currentWord; //api call here for render word
 let currentWordArray;
 let guessedLettersArray = [];
 let livesRemaining = 6; //lives that are left in game
-let remainingLetters = currentWord.count; //letters left to be guessed in word
+let remainingLetters; //letters left to be guessed in word
 let wordLibrary;
 let wordLibraryArray;
 
-let result = document.getElementById("answerArray");
+let result = document.getElementById("status");
 
 
 //const underscores = document.getElementById('underscores')
@@ -36,17 +36,15 @@ let result = document.getElementById("answerArray");
 //API call for word library
 //create a request variable
 let request = new XMLHttpRequest();
-request.open('GET', 'localhost:9001/words', true);
+request.open('GET', 'http://localhost:9001/words', true);
 request.withCredentials = "true";
 //running into CORS issues, so i can make the request and have it be on the same origin
-request.onload = function () {
+request.onload = function() {
   // Begin accessing JSON data here
    wordLibrary = this.response;
-   wordLibraryArray = wordLibrary.split("/n")
+   wordLibraryArray = wordLibrary.split('\n');
   if (request.status >= 200 && request.status < 400) {
     chooseRandomWord();
-    console.log(currentWord)
-
   } else {
     console.log('error');
   }
@@ -57,29 +55,31 @@ request.send();
 // underscores.innerHTML = currentWord.count
 
 //1. choosing random word and returns currentWord as string
-function chooseRandomWord(){
+function chooseRandomWord() {
   //store random word
   currentWord = wordLibraryArray[Math.floor(Math.random() * wordLibraryArray.length)];
   currentWordArray = currentWord.split('');
+  remainingLetters = currentWord.length;
   displayStatus(currentWord);
 }
 
 //shows underscores or letters guessed correctly on game board
-function displayStatus(word){
+function displayStatus(word) {
   let answerArray = [];
   for (let i = 0; i < currentWordArray.length; i++){
-    if (letterGuessed.includes(currentWordArray[i])) {
+    if (guessedLettersArray.includes(currentWordArray[i])) {
       answerArray[i] = currentWordArray[i];
     } else {
       answerArray[i] = "_";
     }
   }
-  return answerArray.innerHTML;
+  return result.innerHTML = answerArray;
 }
+
 
 //note:put take guesss somewhere
 //2. take user's guess
-function takeGuess(letter){
+function takeGuess(letter) {
   //assume i have an input box or buttons
   guessedLettersArray.push(letter);
   isCorrectGuess(letter)
@@ -95,7 +95,7 @@ function isCorrectGuess(letter) {
   // takes in two arguments
   //
   //if false, this should also decrement lives
-  if !(currentWordArray.includes(letterGuessed)) {
+  if (!currentWordArray.includes(letter)) {
     livesRemaining -= 1;
   } else {
     remainingLetters -= 1;
@@ -103,12 +103,16 @@ function isCorrectGuess(letter) {
   }
 }
 
-function endGame(endGamePhrase){
+function endGame(endGamePhrase) {
   //render 'play again' button
-  return endGamePhrase;
+  console.log(endGamePhrase)
+  let gameEnding = document.getElementById("endOfGame");
+  gameEnding.innerHTML = `${endGamePhrase}`;
+  playAgain();
+
 }
 
-function playAgain(){
+function playAgain() {
   //reset all variables and stored letters arrays
   //need to reset
     //guessedLettersArray
@@ -119,6 +123,28 @@ function playAgain(){
   livesRemaining = 6;
   guessedLettersArray = [];
   chooseRandomWord(); //setting currentWord to a new random word
-  currentWordArray
-
+  currentWordArray;
+  //render play again button
 }
+
+//render buttons for game board
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'w', 'y', 'z'];
+
+// const docFragment = document.createDocumentFragment();
+
+let innerHTML = '';
+
+alphabet.forEach(function(letter) {
+  innerHTML += `<button class="alphabet__letter" onclick="takeGuess('${letter}')">${letter}</button>`;
+});
+
+const alphabetElement = document.querySelector('.alphabet');
+alphabetElement.innerHTML = innerHTML;
+
+
+//need to show livesRemaining
+//need to show letters that were guess aka dim buttons (turn them off)
+//need to show remainingLetters
+//bugs: when i click on the same letter twice, my life decrements, need to disable guessing letter again
+//bugs: lives keep decrementing past 0 End Game
+//need to list incorrect guesses (wrong letter bank)
